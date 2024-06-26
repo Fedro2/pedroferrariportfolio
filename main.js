e21248b356e35253198e71c7af7b250e504b89f7
Pedro Ferari <151549155+Fedro2@users.noreply.github.com>
2024-06-26 02:50:19 +0200

    Update main.js

--- main.js
+++ main.js
@@ -1,2 +1,11 @@
 AOS.init();
 
+document.getElementById('ferrari').addEventListener('mouseover', function() {
+    document.getElementById('ferrari').style.visibility = 'hidden';
+    document.getElementById('ferrari-img').style.display = 'block';
+});
+
+document.getElementById('ferrari-container').addEventListener('mouseout', function() {
+    document.getElementById('ferrari').style.visibility = 'visible';
+    document.getElementById('ferrari-img').style.display = 'none';
+});
